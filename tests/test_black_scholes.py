8c4501831fbb29973b351be50a1960c38030a56f
thatTJguy <timothy.j.childers2@gmail.com>
2025-08-01 13:44:00 -0500

    added 3 tests with mark.parametrize test for put pricing

--- tests/test_black_scholes.py
+++ tests/test_black_scholes.py
@@ -1,5 +1,5 @@
 import pytest
-from black_scholes import calculate_call_price
+from black_scholes import calculate_call_price, calculate_put_price
 
 @pytest.mark.math
 #Arrange
@@ -11,5 +11,19 @@ from black_scholes import calculate_call_price
 def test_call_price(S, X, T, r, sigma, expected):
     # Act
     price = calculate_call_price(S, X, T, r, sigma)
+    # Assert
+    assert round(price, 2) == pytest.approx(expected, abs=.02)
+
+
+#test for put pricing
+@pytest.mark.math
+@pytest.mark.parametrize("S, X, T, r, sigma, expected", [
+    (100.0, 105.0, 1.0, 0.05, 0.2, 8.02),  # in-the money put
+    (50.0, 50.0, 0.5, 0.03, 0.25, 2.52),   # at-the-money put
+    (120.0, 100.0, 2.0, 0.04, 0.15, 1.73) # out-of-the-money put
+])
+def test_put_price(S, X, T, r, sigma, expected):
+    # Act
+    price = calculate_put_price(S, X, T, r, sigma)
     # Assert
     assert round(price, 2) == pytest.approx(expected, abs=.02)
