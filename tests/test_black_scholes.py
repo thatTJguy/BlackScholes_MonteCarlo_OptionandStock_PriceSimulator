9221dba3ea760d4892dea7cece0f76cf9ca11eba
thatTJguy <timothy.j.childers2@gmail.com>
2025-08-01 15:56:33 -0500

    added __init__.py to test folder and updated expected calculate_put_price values -old numbers were calculated with tables causing false fails

--- tests/test_black_scholes.py
+++ tests/test_black_scholes.py
@@ -2,7 +2,8 @@ import pytest
 from black_scholes import calculate_call_price, calculate_put_price
 
 @pytest.mark.math
-#Arrange
+#arrange is handled by pytest.mark.parametrize
+#test for call pricing
 @pytest.mark.parametrize("S, X, T, r, sigma, expected", [
     (100.0, 95.0, 1.0, 0.05, 0.2, 13.35),  # Example case
     (50.0, 50.0, 0.5, 0.03, 0.25, 3.89),   # even with same strike price, different time and volatility
@@ -18,9 +19,9 @@ def test_call_price(S, X, T, r, sigma, expected):
 #test for put pricing
 @pytest.mark.math
 @pytest.mark.parametrize("S, X, T, r, sigma, expected", [
-    (100.0, 105.0, 1.0, 0.05, 0.2, 8.02),  # in-the money put
-    (50.0, 50.0, 0.5, 0.03, 0.25, 2.52),   # at-the-money put
-    (120.0, 100.0, 2.0, 0.04, 0.15, 1.73) # out-of-the-money put
+    (100.0, 105.0, 1.0, 0.05, 0.2, 7.90),  # in-the money put
+    (50.0, 50.0, 0.5, 0.03, 0.25, 3.14),   # at-the-money put
+    (120.0, 100.0, 2.0, 0.04, 0.15, 1.16) # out-of-the-money put
 ])
 def test_put_price(S, X, T, r, sigma, expected):
     # Act
